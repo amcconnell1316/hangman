3a2b6c428dee060defe880e2039c801a2ef2eac6
Amanda <125668669+amcconnell1316@users.noreply.github.com>
2023-11-06 08:25:54 -0600

    refactor gamestate into it's own class

---
+++ gamestate.rb
@@ -0,0 +1,51 @@
+class HangmanGamestate
+  attr_reader :word
+
+  def initialize(word)
+    super()
+    @word = word
+    @prev_guesses = []
+    @num_mistakes_left = 15
+  end
+
+  def display
+    display = ''
+    @word.each_char do |char|
+      if @prev_guesses.include?(char)
+        display += char
+      else
+        display += '_'
+      end
+      display += " "
+    end
+    puts display
+    puts "Guessed so far: #{@prev_guesses.join}"
+    puts "Number of mistakes left: #{@num_mistakes_left}"
+  end
+
+  def valid_guess?(guess)
+    return guess.match(/[a-z]/) && !@prev_guesses.include?(guess)
+  end
+
+  def guess_letter(guess)
+    @prev_guesses << guess
+    if @word.include?(guess)
+      puts "That's a correct guess!"
+    else
+      puts("#{guess} was not in the word")
+      @num_mistakes_left -= 1
+    end
+  end
+
+  def game_over?
+    return true if @num_mistakes_left == 0
+    @word.each_char do |char|
+      return false unless @prev_guesses.include?(char)
+    end
+    return true # All characters have been guessed
+  end
+
+  def game_won?
+    @num_mistakes_left > 0
+  end
+end
--- hangman.rb
+++ hangman.rb
@@ -1,59 +1,31 @@
+require_relative 'gamestate'
 
-def display_guesses(word, guessed_letters, num_mistakes_left)
-  display = ''
-  word.each_char do |char|
-    if guessed_letters.include?(char)
-      display += char
-    else
-      display += '_'
-    end
-    display += " "
-  end
-  puts display
-  puts "Guessed so far: #{guessed_letters.join}"
-  puts "Number of mistakes left: #{num_mistakes_left}"
-end
-
-def get_guess(prev_guesses)
+def get_guess(gamestate)
   guess = ''
   loop do 
     puts 'Guess a letter: '
     guess = gets.chomp.downcase[0]
-    break if guess.match(/[a-z]/) && !prev_guesses.include?(guess)
+    break if gamestate.valid_guess?(guess) 
   end
   guess
 end
 
-def game_over(word, guessed_letters, num_mistakes_left)
-  return true if num_mistakes_left == 0
-  word.each_char do |char|
-    return false unless guessed_letters.include?(char)
-  end
-  return true # All characters have been guessed
-end
 
 dictionary = File.readlines('google-10000-english-no-swears.txt', chomp: true)
 dictionary = dictionary.select {|word| word.length >=5 && word.length <=12}
 
-word = dictionary.sample
-guesses = []
-num_mistakes_left = 15
-while num_mistakes_left > 0 && !game_over(word, guesses, num_mistakes_left) do
-  display_guesses(word, guesses, num_mistakes_left)
-  guess = get_guess(guesses)
-  guesses << guess
-  if word.include?(guess)
-    puts "That's a correct guess!"
-  else
-    puts("#{guess} was not in the word")
-    num_mistakes_left -= 1
-  end
+gamestate = HangmanGamestate.new(dictionary.sample)
+
+while !gamestate.game_over? do
+  gamestate.display
+  guess = get_guess(gamestate)
+  gamestate.guess_letter(guess)
 end
 
-if num_mistakes_left > 0
-  puts "You guessed the whole word! #{word}"
+if gamestate.game_won?
+  puts "You guessed the whole word! #{gamestate.word}"
 else
-  puts "You ran out of guesses. The word was #{word}"
+  puts "You ran out of guesses. The word was #{gamestate.word}"
 end
 
 
